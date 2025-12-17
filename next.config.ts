f75ed96d0725e9fe9c285cef3777820d62e252d5
Robin Pellegrims <robin@pellegrims.dev>
2025-12-17 21:11:56 +0100

    fix technical issue with hmr

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
+  experimental: {
+    optimizePackageImports: ["framer-motion"],
+  },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
